02d59847bbe430ab2c72ee38f1462914735e5405
DinithiPramodya <dinithi251@gmail.com>
2026-08-31 17:22:12 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -10,33 +10,50 @@ import (
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Print
 
+//Checks whether a command prefixed 'type' is a builtin command or not(unrecognizable)
+func isShellBuiltin(command string ) bool {
+	if command == "echo" || command == "exit" || command == "type" {
+		return true
+	} else {
+		return false
+	}
+
+}
+
 func main() {
 
 	reader := bufio.NewReader(os.Stdin)
 	for {
-	// TODO: Uncomment the code below to pass the first stage
-	fmt.Print("$ ")
+		// TODO: Uncomment the code below to pass the first stage
+		fmt.Print("$ ")
 
-	command, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Println("Error reading input:", err)
-	}
+		command, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading input:", err)
+		}
 
-	command = strings.TrimSpace(command)
+		command = strings.TrimSpace(command)
 
-	//builtin exit command
-	if command == "exit" {
-		break
+		//builtin exit command
+		if command == "exit" {
+			break
 
-	//builtin echo command
-	}else if strings.HasPrefix(command, "echo ") {
-		fmt.Println(command[5:])
-		
-	}else {
-		fmt.Println(command + ": command not found")
-	}
-	// command = strings.TrimRight(command, "\r\n")
-	// fmt.Println(command + ": command not found")
+		//builtin echo command
+		}else if strings.HasPrefix(command, "echo ") {
+			fmt.Println(command[5:])
+
+		//outputs the type of the command 
+		}else if strings.HasPrefix(command, "type") {
+			if isShellBuiltin(command[5:]) {
+				fmt.Println(command[5:] + " is a shell builtin")
+			} else {
+				fmt.Println(command[5:] + ": not found")
+			}
+		}else {
+			fmt.Println(command + ": command not found")
+		}
+		// command = strings.TrimRight(command, "\r\n")
+		// fmt.Println(command + ": command not found")
 
 	}
 
